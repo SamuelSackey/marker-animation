1b69c03cc54a97d32d63327ab99b435033a786b2
Samuel Sackey <smlscky@gmail.com>
2023-09-19 14:08:50 +0000

    header change

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const markers = document.querySelectorAll(".marker");
+const header = document.querySelector(".header");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       const element = entry.target;
       const elementStyle = element.style;
+
+      header.style.setProperty(
+        "--header-color",
+        getComputedStyle(element).getPropertyValue("--header-change")
+      );
+
       elementStyle.setProperty("--marker-opacity", "1");
       elementStyle.setProperty("--marker-width", "100%");
     }
